ca0d21ae810f7f87dc51d9441dec6aea2dfcd959
DanielSobrino <dsobrino343@gmail.com>
2022-07-12 00:16:34 +0200

    feat: stats cmd optional target

--- commands/stats.js
+++ commands/stats.js
@@ -3,10 +3,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('stats')
-        .setDescription('Check your wordle stats (TODO)'),
+        .setDescription("Check the user's wordle stats (TODO)")
+        .addUserOption((option) =>
+            option.setName('target').setDescription("The user's stats to show")
+        ),
     async execute(interaction) {
-        await interaction.reply(
-            `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+        const user = interaction.options.getUser('target');
+        if (user)
+            return interaction.reply(
+                `${user.username}'s stats:\nId: ${user.id}\nTag: ${user.tag}`
+            );
+        return interaction.reply(
+            `Your id: ${interaction.user.id}\nYour tag: ${interaction.user.tag}`
         );
     },
 };
